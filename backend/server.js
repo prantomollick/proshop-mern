a12d21f199e19d8d0f4d106fadcc61b3e7c58845
pranto mollick <prantocse01@gmail.com>
2022-12-23 21:00:36 +0600

    007 Fetching Products From The Database

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -14,14 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is Running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
