94f2edc3761c3c5f59917211da32ec87fbc548d7
pranto mollick <prantocse01@gmail.com>
2022-12-23 22:09:57 +0600

    003 Product List Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,11 @@ import {
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({});
+import { productListReducer } from './reducers/productReducers';
+
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 const initialState = {};
 const middleware = [thunk];
 
