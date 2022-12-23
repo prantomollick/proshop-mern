e70657255b46fba13cfa7d1119eb5c739aed027d
pranto mollick <prantocse01@gmail.com>
2022-12-23 21:45:41 +0600

    create redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './bootstrap.min.css';
@@ -31,7 +34,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
