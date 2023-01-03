dec87a8c2046332d9caeef3762d8c80284e70a14
pranto mollick <prantocse01@gmail.com>
2023-01-03 17:33:24 +0600

    update to paid Endpoint

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -23,8 +23,8 @@ const OrderScreen = () => {
   }
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
-  }, [orderId, dispatch]);
+    if (!order || order._id !== orderId) dispatch(getOrderDetails(orderId));
+  }, [orderId, dispatch, order]);
 
   return loading ? (
     <Loader />
